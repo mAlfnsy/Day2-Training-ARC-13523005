cf617b392a3d69de5056763b229d99d45c582c5b
malfansya <muhammadalfansya@gmail.com>
2025-02-28 23:35:32 +0700

    feat : add view product info

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const response = await fetch("https://dummyjson.com/products?limit=10");
         const data = await response.json();
         const products = data.products;
         const productList = document.getElementById("product-list");
+        const closeInfo = document.getElementById("close-info");
+        
+        function showProductInfo(product) {
+            document.getElementById("info-img").src = product.thumbnail;
+            document.getElementById("info-title").textContent = product.title;
+            document.getElementById("info-description").textContent = product.description;
+            document.getElementById("info-price").textContent = product.price;
+            document.getElementById("info-rating").textContent = product.rating;
+            document.getElementById("product-info").style.display = "block";
+        }
         
         products.forEach(product => {
             const productCard = document.createElement("div");
@@ -21,6 +32,9 @@ document.addEventListener("DOMContentLoaded", async () => {
             productList.appendChild(productCard);
         });
         
+        closeInfo.addEventListener("click", () => {
+            document.getElementById("product-info").style.display = "none";
+        });
     } catch (error) {
         console.error("Gagal mengambil data produk", error);
     }
